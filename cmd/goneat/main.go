9de97a3ceb5f03fed2a01563ce65cc0e0e79f0e9
Clem Davies <github@clemdavies.com>
2022-02-20 12:14:38 +0800

    init commit

---
+++ cmd/goneat/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	output("NEATGO: start")
+}
+
+func output(str string) {
+	fmt.Println(str)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/clemdavies/neatgo
+
+go 1.17
